d406aada36a002b9cf98b737dd3c542c6f81ef00
Kevin Tipantiza <kevinftipantiza@gmail.com>
2021-10-21 13:39:48 -0500

    preparando el proyecto

---
+++ src/Components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <nav className="navbar navbar-light bg-dark">
+        <div className="container-fluid">
+          <span className="navbar-brand mb-0 h1 text-white">CONTEXT</span>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Header;
