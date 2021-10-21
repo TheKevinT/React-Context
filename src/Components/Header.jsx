05e3aa66d2c413fd8b3a20fe9ff0ec86aaa262a7
Kevin Tipantiza <kevinftipantiza@gmail.com>
2021-10-21 15:05:53 -0500

    navbar

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,9 +3,34 @@ import React from "react";
 const Header = () => {
   return (
     <>
-      <nav className="navbar navbar-light bg-dark">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <span className="navbar-brand mb-0 h1 text-white">CONTEXT</span>
+          <button
+            type="button"
+            className="navbar-toggler"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
+              <li className="navbar-item">
+                <a href="#" className="nav-link active">
+                  Home
+                </a>
+              </li>
+              <li className="navbar-item">
+                <a href="#" className="nav-link">
+                  About
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     </>
